adafc5c9f46f37e17c89bf4966ac73d1cc1b70a9
ggunnim <ggunnim@naver.com>
2018-10-16 01:00:34 -0700

    branch I did

--- myapp.c
+++ myapp.c
@@ -2,10 +2,24 @@
 #include "func1.h"
 #include "func2.h"
 
+int turbo_on()
+{
+  printf("turbo_on\n");
+}
+
+int turbo_off()
+{
+  printf("turbo_off\n");
+}
+
 int main()
 {
+  turbo_on();
+
   func1();
   func2();
+
+  turbo_off();
 
   return 0;
 }
